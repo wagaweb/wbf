f8d5366936c8e33853e576e11570392a6843579d
LostCore <lostcore@gmail.com>
2015-09-24 14:36:26 +0200

    Utilizzato wbf_locate_file

--- wbf-autoloader.php
+++ wbf-autoloader.php
@@ -9,107 +9,90 @@ spl_autoload_register('wbf_autoloader');
  * @param $class
  * @since 0.1.4
  */
-function wbf_autoloader($class)
-{
+function wbf_autoloader($class) {
+
     //Load Options Framework Classes
     if (preg_match("/^Options_Framework_/", $class)) {
         $filename = "class-" . strtolower(preg_replace("/_/", "-", $class)) . ".php";
         if ($class == "Options_Framework_Admin") {
-	        require_once WBF_DIRECTORY.'/vendor/options-framework/' . $filename;
+	        wbf_locate_file('vendor/options-framework/' . $filename, true);
         } else {
             $filename = preg_replace("/-framework/", "", $filename);
-	        require_once WBF_DIRECTORY.'/vendor/options-framework/' . $filename;
+	        wbf_locate_file('vendor/options-framework/' . $filename, true);
         }
     }
-
-    /*if (preg_match("/^Waboot_Options_/", $class)) {
-        $filename = "class-" . strtolower(preg_replace("/_/", "-", $class)) . ".php";
-        locate_template('wbf/admin/' . $filename, true);
-    }*/
 
     if (preg_match("/conditions/", $class)) {
         $childclass = explode('\\', $class);
         $name = end($childclass);
-	    require_once WBF_DIRECTORY.'/admin/conditions/'.$name.'.php';
+	    wbf_locate_file('admin/conditions/'.$name.'.php', true);
     }
 
     if (preg_match("/modules/", $class)) {
         $childclass = explode('\\', $class);
         $name = end($childclass);
         $module = $childclass[2];
-        require_once WBF_DIRECTORY.'/modules/'.$module.'/'.$name.'.php';
+	    wbf_locate_file('modules/'.$module.'/'.$name.'.php', true);
     }
 
     switch ($class) {
-	    case "WBF\includes\License_Interface":
-		    require_once WBF_DIRECTORY.'/includes/license-interface.php';
+	    case 'WBF\includes\License_Interface':
+		    wbf_locate_file('includes/license-interface.php', true);
 		    break;
-	    case "WBF\includes\License":
-		    require_once WBF_DIRECTORY.'/includes/class-license.php';
+	    case 'WBF\includes\License':
+		    wbf_locate_file('includes/class-license.php', true);
 		    break;
-	    case "WBF\includes\License_Exception":
-		    require_once WBF_DIRECTORY.'/includes/class-license-exception.php';
+	    case 'WBF\includes\License_Exception':
+		    wbf_locate_file('includes/class-license-exception.php', true);
 		    break;
-        case "WBF\admin\License_Manager":
-	        require_once WBF_DIRECTORY.'/admin/license-manager.php';
+        case 'WBF\admin\License_Manager':
+	        wbf_locate_file('admin/license-manager.php', true);
             break;
-        case "WBF\admin\Notice_Manager":
-	        require_once WBF_DIRECTORY.'/admin/notice-manager.php';
+        case 'WBF\admin\Notice_Manager':
+	        wbf_locate_file('admin/notice-manager.php', true);
             break;
-        case "WBF\includes\Plugin_Update_Checker":
-	        require_once WBF_DIRECTORY.'/includes/plugin-update-checker.php';
+        case 'WBF\includes\Plugin_Update_Checker':
+	        wbf_locate_file('includes/plugin-update-checker.php', true);
             break;
-	    case "WBF\includes\Theme_Update_Checker":
-		    require_once WBF_DIRECTORY.'/includes/theme-update-checker.php';
+	    case 'WBF\includes\Theme_Update_Checker':
+		    wbf_locate_file('includes/theme-update-checker.php', true);
 		    break;
 	    case 'WBF\includes\compiler\Styles_Compiler':
-		    require_once WBF_DIRECTORY.'/includes/compiler/class-styles-compiler.php';
+		    wbf_locate_file('includes/compiler/class-styles-compiler.php', true);
 		    break;
 	    case 'WBF\includes\compiler\Base_Compiler':
-		    require_once WBF_DIRECTORY.'/includes/compiler/interface-base-compiler.php';
+		    wbf_locate_file('includes/compiler/interface-base-compiler.php', true);
 		    break;
         case 'WBF\includes\compiler\less\Less_Cache':
-	        require_once WBF_DIRECTORY.'/includes/compiler/less/Less_Cache.php';
+	        wbf_locate_file('includes/compiler/less/Less_Cache.php', true);
             break;
         case 'WBF\includes\compiler\less\Less_Compiler':
-	        require_once WBF_DIRECTORY.'/includes/compiler/less/Less_Compiler.php';
+	        wbf_locate_file('includes/compiler/less/Less_Compiler.php', true);
             break;
         case "Less_Cache":
-	        require_once WBF_DIRECTORY.'/includes/compiler/less/vendor/Lessphp/Cache.php';
+	        wbf_locate_file('includes/compiler/less/vendor/Lessphp/Cache.php', true);
             break;
         case "Less_Parser":
-	        require_once WBF_DIRECTORY.'/includes/compiler/less/vendor/Lessphp/Less.php';
+	        wbf_locate_file('includes/compiler/less/vendor/Lessphp/Less.php', true);
             break;
         case "lessc":
-	        require_once WBF_DIRECTORY.'/includes/compiler/less/vendor/Lessphp/lessc.inc.php';
+	        wbf_locate_file('includes/compiler/less/vendor/Lessphp/lessc.inc.php', true);
             break;
         case "Less_Version":
-	        require_once WBF_DIRECTORY.'/includes/compiler/less/vendor/Lessphp/Version.php';
+	        wbf_locate_file('includes/compiler/less/vendor/Lessphp/Version.php', true);
             break;
         case "BootstrapNavMenuWalker":
-	        require_once WBF_DIRECTORY.'/vendor/BootstrapNavMenuWalker.php';
+	        wbf_locate_file('vendor/BootstrapNavMenuWalker.php', true);
             break;
         case "WabootNavMenuWalker":
-	        require_once WBF_DIRECTORY.'/public/menu-navwalker.php';
+	        wbf_locate_file('public/menu-navwalker.php', true);
             break;
         case "ThemeUpdate":
         case "ThemeUpdateChecker":
-	        require_once WBF_DIRECTORY.'/vendor/theme-updates/theme-update-checker.php';
+	        wbf_locate_file('vendor/theme-updates/theme-update-checker.php', true);
             break;
-        /*case "PluginUpdateChecker":
-        case "PluginUpdate":
-        case "PluginInfo":
-        case "PluginUpdateChecker_1_6":
-        case "PluginInfo_1_6":
-        case "PluginUpdate_1_6":
-        case "PucFactory":
-            locate_template('wbf/vendor/plugin-updates/plugin-update-checker.php', true);
-            break;*/
-        /*case "Mobile_Detect":
-            locate_template('wbf/vendor/Mobile_Detect.php', true);
-            break;*/
         case "Options_Framework":
-	        require_once WBF_DIRECTORY.'/vendor/options-framework/class-options-framework.php';
+	        wbf_locate_file('vendor/options-framework/class-options-framework.php', true);
             break;
     }
 }
