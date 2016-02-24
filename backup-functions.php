2a22afae00e68a0b63f37acde875c458686054d7
lostcore@gmail.com <123qw45>
2016-02-24 16:30:29 +0100

    Ripristinate backup functions, dovranno essere spostate

--- backup-functions.php
+++ backup-functions.php
@@ -4,29 +4,36 @@
  * Backward compatibility for wbf_breadcrumb_trail
  * @param array $args
  */
-/*function waboot_breadcrumb_trail( $args = array() ){
-	wbf_breadcrumb_trail($args);
-}
-*/
 
-/**
- * Behaviors framework backup functions; handles the case in which the Behaviors are not loaded
- *
- * @param $name
- * @param int $post_id
- * @param string $return
- *
- * @return array|bool|mixed|string
- */
-/*
-function get_behavior( $name, $post_id = 0, $return = "value" ) {
-    if (class_exists('\WBF\modules\behaviors\BehaviorsManager')) {
-        return \WBF\modules\behaviors\get_behavior( $name, $post_id, $return = "value" ); //call the behavior framework function
-    } else {
-        return WBF::get_behavior( $name, $post_id, $return = "value" ); //call the backup function
-    }
-}
-*/
+if(!function_exists("waboot_breadcrumb_trail")):
+	/**
+	 *
+	 *
+	 * @param array $args
+	 */
+	function waboot_breadcrumb_trail( $args = array() ){
+		wbf_breadcrumb_trail($args);
+	}
+endif;
+
+if(!function_exists("get_behavior")):
+	/**
+	 * Behaviors framework backup functions; handles the case in which the Behaviors are not loaded
+	 *
+	 * @param $name
+	 * @param int $post_id
+	 * @param string $return
+	 *
+	 * @return array|bool|mixed|string
+	 */
+	function get_behavior( $name, $post_id = 0, $return = "value" ) {
+		if (class_exists('\WBF\modules\behaviors\BehaviorsManager')) {
+			return \WBF\modules\behaviors\get_behavior( $name, $post_id, $return = "value" ); //call the behavior framework function
+		} else {
+			return WBF::get_behavior( $name, $post_id, $return = "value" ); //call the backup function
+		}
+	}
+endif;
 
 if(!function_exists("of_get_option")):
 	/**
@@ -40,16 +47,16 @@ if(!function_exists("of_get_option")):
 	}
 endif;
 
-/**
- *
- * @param $name
- * @return bool
- */
-/*
-function component_is_loaded($name){
-    if(class_exists('\WBF\modules\components\ComponentsManager')) {
-        return \WBF\modules\components\ComponentsManager::is_loaded_by_name($name);
-    }
-    return false;
-}
-*/
+if(!function_exists("component_is_loaded")):
+	/**
+	 *
+	 * @param $name
+	 * @return bool
+	 */
+	function component_is_loaded($name){
+		if(class_exists('\WBF\modules\components\ComponentsManager')) {
+			return \WBF\modules\components\ComponentsManager::is_loaded_by_name($name);
+		}
+		return false;
+	}
+endif;
