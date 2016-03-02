2b5bf5aee400ecb43d1a62cca13fc52a4ccf4573
lostcore@gmail.com <123qw45>
2016-03-02 16:37:26 +0100

    Inserito FUTURO PSR-4 Loader

--- wbf-autoloader.php
+++ wbf-autoloader.php
@@ -2,6 +2,29 @@
 
 require_once("vendor/autoload.php");
 
+// FUTURE PSR4 Custom plugin autoloader function
+/*spl_autoload_register( function($class){
+	$prefix = "WBF\\";
+	$plugin_path = plugin_dir_path( __FILE__ );
+	$base_dir = $plugin_path."src/";
+	// does the class use the namespace prefix?
+	$len = strlen($prefix);
+	if (strncmp($prefix, $class, $len) !== 0) {
+		// no, move to the next registered autoloader
+		return;
+	}
+	// get the relative class name
+	$relative_class = substr($class, $len);
+	// replace the namespace prefix with the base directory, replace namespace
+	// separators with directory separators in the relative class name, append
+	// with .php
+	$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+	// if the file exists, require it
+	if (file_exists($file)) {
+		require_once $file;
+	}
+});*/
+
 spl_autoload_register('wbf_autoloader');
 
 /**
