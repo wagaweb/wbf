4a94b9c152c1d42575522430e4b419ab39b6d926
lostcore@gmail.com <123qw45>
2016-04-04 11:14:58 +0200

    Aggiunta versione dei file

--- admin/adm-scripts.php
+++ admin/adm-scripts.php
@@ -1,18 +1,27 @@
 <?php
 
+/**
+ * Register wbf admin scripts
+ */
 function wbf_admin_scripts(){
-    if(WBF_ENV == "dev"){
-	    wp_register_script("wbf-admin",WBF_URL."/assets/src/js/admin/wbf-admin-bundle.js",array("jquery","backbone","underscore"),false,true);
+	$deps = apply_filters("wbf/js/admin/deps",["jquery","backbone","underscore"]);
+    if(defined("SCRIPT_DEBUG") && SCRIPT_DEBUG){
+		$file_path = WBF_DIRECTORY."/assets/src/js/admin/wbf-admin-bundle.js";
+		$file_url = WBF_URL."/assets/src/js/admin/wbf-admin-bundle.js";
     }else{
-        wp_register_script("wbf-admin",WBF_URL."/admin/js/wbf-admin.min.js",array("jquery","backbone","underscore"),false,true);
+		$file_path = WBF_DIRECTORY."/admin/js/wbf-admin.min.js";
+		$file_url = WBF_URL."/admin/js/wbf-admin.min.js";
     }
-	$wbfData = apply_filters("wbf/js/admin/localization",[
-		'ajaxurl' => admin_url('admin-ajax.php'),
-		'wpurl' => get_bloginfo('wpurl'),
-		'wp_screen' => function_exists("get_current_screen") ? get_current_screen() : null,
-		'isAdmin' => is_admin()
-	]);
-	wp_localize_script("wbf-admin","wbfData",$wbfData);
-	wp_enqueue_script("wbf-admin");
+	if(is_file($file_path)){
+		wp_register_script("wbf-admin",$file_url,$deps,filemtime($file_path),true);
+		$wbfData = apply_filters("wbf/js/admin/localization",[
+			'ajaxurl' => admin_url('admin-ajax.php'),
+			'wpurl' => get_bloginfo('wpurl'),
+			'wp_screen' => function_exists("get_current_screen") ? get_current_screen() : null,
+			'isAdmin' => is_admin()
+		]);
+		wp_localize_script("wbf-admin","wbfData",$wbfData);
+		wp_enqueue_script("wbf-admin");
+	}
 }
 add_action( 'admin_enqueue_scripts', 'wbf_admin_scripts', 99);
