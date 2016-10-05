d32b21e9e8650c6ecef8eed825cab91e5c25128f
lostcore@gmail.com <123qw45>
2016-10-05 13:10:22 +0200

    Rimosse le funzioni ora delegate alla GUI Aggiunte azioni per l'update e l'add della Licenza

--- src/components/license/License_Manager.php
+++ src/components/license/License_Manager.php
@@ -2,85 +2,7 @@
 
 namespace WBF\components\license;
 
-use WBF\components\license\License;
-use WBF\components\license\License_Exception;
-use WBF\components\mvc\HTMLView;
-use WBF\components\customupdater\Theme_Update_Checker;
-use WBF\components\utils\Utilities;
-
 class License_Manager{
-
-	static function init(){
-		add_action( 'current_screen', 'WBF\admin\License_Manager::perform_page_actions', 10 );
-		add_action( 'wbf_admin_submenu', 'WBF\admin\License_Manager::admin_license_menu_item', 30 );
-	}
-
-	static function perform_page_actions(){
-		if(!preg_match("/wbf_licenses/",get_current_screen()->base)){
-			return;
-		}
-		$wbf_notice_manager = Utilities::get_wbf_notice_manager();
-		if(isset($_POST['update-license'])){
-			try{
-				if(!isset($_POST['slug'])) throw new License_Exception(__("License slug was not set","wbf"));
-				if(!isset($_POST['type'])) throw new License_Exception(__("License type was not set","wbf"));
-				if(!isset($_POST['code']) || empty($_POST['code'])) throw new License_Exception(__("License code was not set","wbf"));
-				$slug = $_POST['slug'];
-				$type = $_POST['type'];
-				$code = trim($_POST['code']);
-				self::update($slug,$type,$code);
-				$wbf_notice_manager->add_notice("license_updated",_x("License updated!","License","wbf"),"updated","_flash_");
-			}catch(License_Exception $e){
-				$wbf_notice_manager->add_notice("license_not_updated",$e->getMessage(),"error","_flash_");
-			}
-		}elseif(isset($_POST['delete-license'])){
-			try{
-				if(!isset($_POST['slug'])) throw new License_Exception(__("License slug was not set","wbf"));
-				if(!isset($_POST['type'])) throw new License_Exception(__("License type was not set","wbf"));
-				$slug = $_POST['slug'];
-				$type = $_POST['type'];
-				self::delete($slug,$type);
-				$wbf_notice_manager->add_notice("license_deleted",_x("License deleted!","License","wbf"),"updated","_flash_");
-			}catch(License_Exception $e){
-				$wbf_notice_manager->add_notice("license_not_deleted",$e->getMessage(),"error","_flash_");
-			}
-		}
-	}
-
-	static function admin_license_menu_item($parent_slug){
-		$licenses = self::get_all();
-		if(is_array($licenses) && !empty($licenses)){
-			add_submenu_page( $parent_slug, __( "Licenses", "wbf" ), __( "Licenses", "wbf" ), "edit_theme_options", "wbf_licenses", "WBF\admin\License_Manager::license_page" );
-		}
-	}
-
-	/**
-	 * Callback for displaying the licenses page
-	 */
-	static function license_page(){
-		$v = new HTMLView("src/views/admin/license-manager.php","wbf");
-		$vars = [
-			'has_theme_licenses' => self::has_theme_licenses(),
-			'has_plugin_licenses' => self::has_plugin_licenses(),
-			'theme_licenses' => self::has_theme_licenses() ? self::get_theme_licenses() : [],
-			'plugin_licenses' => self::has_plugin_licenses() ? self::get_plugin_licenses() : [],
-		];
-		$v->clean()->display($vars);
-	}
-
-	/**
-	 * Hides the first characters of a license code
-	 * @param     $code
-	 * @param int $cut_point
-	 *
-	 * @return string
-	 */
-	static function crypt_license_visual($code,$cut_point = 4){
-		$first_chars = substr($code,0,strlen($code)-$cut_point);
-		$first_chars = preg_replace("|[\\w]|","*",$first_chars);
-		$last_chars = substr($code,strlen($code)-$cut_point);
-		return $first_chars.$last_chars;
-	}
 
 	static function get($license_slug,$type){
 		$licenses = self::get_all();
@@ -147,6 +69,7 @@ class License_Manager{
 		$value = $l::sanitize_license($value);
 		if($value && is_string($value)){
 			$l->update($value);
+			do_action("wbf/license_updated",$l);
 		}else{
 			throw new License_Exception(__("License sanitization has gone wrong","wbf"));
 		}
@@ -160,6 +83,7 @@ class License_Manager{
 	static function delete($license_slug,$type){
 		$l = self::get($license_slug,$type);
 		$l->remove();
+		do_action("wbf/license_removed",$l);
 	}
 
 	/**
@@ -212,5 +136,19 @@ class License_Manager{
 	 */
 	static function theme_has_license($theme_slug){
 		return self::get($theme_slug,"theme");
+	}
+
+	/**
+	 * Hides the first characters of a license code
+	 * @param     $code
+	 * @param int $cut_point
+	 *
+	 * @return string
+	 */
+	static function crypt_license_visual($code,$cut_point = 4){
+		$first_chars = substr($code,0,strlen($code)-$cut_point);
+		$first_chars = preg_replace("|[\\w]|","*",$first_chars);
+		$last_chars = substr($code,strlen($code)-$cut_point);
+		return $first_chars.$last_chars;
 	}
 }
