498c34b836ece0f612694a5eff9323cc07a30e71
LostCore <lostcore@gmail.com>
2016-11-29 16:43:53 +0100

    Removed

--- src/extensions/cmb2/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace WBF\extensions\cmb2;
-/**
- * Get the bootstrap!
- */
-if ( file_exists(  __DIR__ . '/CMB2Plugin/init.php' ) ) {
-	include_once  __DIR__ . '/CMB2Plugin/init.php';
-}
