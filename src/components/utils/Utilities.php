03e95cdaab56c849cdc1f8ae823b4ffb1f8e106e
LostCore <lostcore@gmail.com>
2016-12-01 11:51:35 +0100

    Added recursive_array_diff and recursive_array_diff_assoc

--- src/components/utils/Utilities.php
+++ src/components/utils/Utilities.php
@@ -824,6 +824,70 @@ class Utilities{
 	}
 
 	/**
+	 * Recursive version of array_diff
+	 *
+	 * @link http://stackoverflow.com/questions/3876435/recursive-array-diff
+	 *
+	 * @param array $arr1
+	 * @param array $arr2
+	 *
+	 * @return array
+	 */
+	static function recursive_array_diff($arr1, $arr2) {
+		$outputDiff = [];
+		foreach ($arr1 as $key => $value) {
+			//if the key exists in the second array, recursively call this function
+			//if it is an array, otherwise check if the value is in arr2
+			if (array_key_exists($key, $arr2)) {
+				if (is_array($value)) {
+					$recursiveDiff = self::recursive_array_diff($value, $arr2[$key]);
+					if (count($recursiveDiff)) {
+						$outputDiff[$key] = $recursiveDiff;
+					}
+				}
+				else if (!in_array($value, $arr2)) {
+					$outputDiff[$key] = $value;
+				}
+			}
+			//if the key is not in the second array, check if the value is in
+			//the second array (this is a quirk of how array_diff works)
+			else if (!in_array($value, $arr2)) {
+				$outputDiff[$key] = $value;
+			}
+		}
+		return $outputDiff;
+	}
+
+	/**
+	 * Recursive version of array_diff_assoc
+	 *
+	 * @link https://www.drupal.org/files/1850798-base-array_recurse-drupal-68.patch
+	 *
+	 * @param array $array1
+	 * @param array $array2
+	 *
+	 * @return array
+	 */
+	static function recursive_array_diff_assoc($array1, $array2) {
+		$difference = array();
+		foreach ( $array1 as $key => $value ) {
+			if ( is_array( $value ) ) {
+				if ( ! array_key_exists( $key, $array2 ) || ! is_array( $array2[ $key ] ) ) {
+					$difference[ $key ] = $value;
+				} else {
+					$new_diff = self::recursive_array_diff_assoc( $value, $array2[ $key ] );
+					if ( ! empty( $new_diff ) ) {
+						$difference[ $key ] = $new_diff;
+					}
+				}
+			} elseif ( ! array_key_exists( $key, $array2 ) || $array2[ $key ] !== $value ) {
+				$difference[ $key ] = $value;
+			}
+		}
+		return $difference;
+	}
+
+	/**
 	 * Guess what :)
 	 *
 	 * @param $needle
