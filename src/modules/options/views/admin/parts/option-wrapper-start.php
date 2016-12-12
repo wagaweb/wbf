678f84bfb3903ed46033489c2d0292a195f08042
fabosway <fabio@waga.it>
2016-12-12 10:41:17 +0100

    added data-section

--- src/modules/options/views/admin/parts/option-wrapper-start.php
+++ src/modules/options/views/admin/parts/option-wrapper-start.php
@@ -1,4 +1,4 @@
-<div id="field-option-<?php echo $id ?>" class="field-option field-option-<?php echo $type ?> <?php echo $additional_classes ?>">
+<div id="field-option-<?php echo $id ?>" class="field-option field-option-<?php echo $type ?> <?php echo $additional_classes ?>" data-section="<?php echo $id ?>">
 	<?php if($name): ?>
 		<h4 class="heading"><?php echo $name; ?></h4>
 	<?php endif; ?>
