919247ef3704aa7d078c4442d2180848164ceb5d
lostcore@gmail.com <123qw45>
2017-04-19 11:02:20 +0200

    Fixed class detection based on type

--- src/components/widgets/WBF_Widget.php
+++ src/components/widgets/WBF_Widget.php
@@ -2,9 +2,32 @@
 
 namespace WBF\components\widgets;
 
-
 class WBF_Widget extends \WP_Widget {
 
+	/**
+	 * Return the class name of field type $type
+	 *
+	 * @param $field_type
+	 *
+	 * @return string
+	 * @throws \Exception
+	 */
+	private function get_class_name($field_type){
+		$class_name_prefix = __NAMESPACE__."\\fields\\";
+		$field_type_to_class = [
+			'checkbox' => 'Checkbox',
+			'multicheckbox' => 'MultiCheckbox',
+			'radio' => 'Radio',
+			'select' => 'Select',
+			'text' => 'Text',
+		];
+		if(array_key_exists($field_type,$field_type_to_class)){
+			$class_name = $class_name_prefix.$field_type_to_class[$field_type];
+			return $class_name;
+		}else{
+			throw new \Exception("Invalid field type");
+		}
+	}
 
 	/**
 	 * Print Widgets options in the backend widget form
@@ -13,22 +36,20 @@ class WBF_Widget extends \WP_Widget {
 	 * @param   array   $form_options
 	 */
 	public function print_options($instance, $form_options){
-
 		foreach ($form_options as $slug => $form_option) {
-
-			$class_name = __NAMESPACE__."\\fields\\".strtoupper($form_option['type']);
-
-			if(class_exists($class_name)){
-				$field = [
-					'slug' => $slug,
-					'id' => $this->get_field_id($slug),
-					'name' => $this->get_field_name($slug),
-					'options' => $form_option
-				];
-				$f = new $class_name($instance, $field);
-
-				$f->get_html();
-			}
+			try{
+				$class_name = $this->get_class_name($form_option['type']);
+				if(class_exists($class_name)){
+					$field = [
+						'slug' => $slug,
+						'id' => $this->get_field_id($slug),
+						'name' => $this->get_field_name($slug),
+						'options' => $form_option
+					];
+					$f = new $class_name($instance, $field);
+					$f->get_html();
+				}
+			}catch (\Exception $e){}
 		}
 	}
 }
