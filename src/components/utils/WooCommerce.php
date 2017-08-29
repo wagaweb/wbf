ee38812c0a96cfd4ef2af44a5ab8356e96663af4
LostCore <lostcore@gmail.com>
2017-08-29 15:42:16 +0200

    Enhanced class
    (cherry picked from commit dc7dad5)

--- src/components/utils/WooCommerce.php
+++ src/components/utils/WooCommerce.php
@@ -72,6 +72,55 @@ class WooCommerce{
 	}
 
 	/**
+	 * Get the product attributes
+	 *
+	 * @param int|object $product
+	 *
+	 * @return array
+	 */
+	public static function get_product_attributes($product){
+
+		if(is_numeric($product)){
+			$product = self::get_product($product);
+		}
+
+		$is_variation = $product instanceof \WC_Product_Variation || $product instanceof WBF_Product_Variation;
+
+		$main_attributes = call_user_func(function() use($product){
+			$taxs = wc_get_attribute_taxonomies();
+			$terms = [];
+			foreach ($taxs as $att_tax){
+				$real_tax_name = 'pa_'.$att_tax->attribute_name;
+				if(taxonomy_exists($real_tax_name)){
+					$term_list = wp_get_post_terms($product->id,$real_tax_name);
+					if(is_array($term_list) && !empty($term_list)){
+						$terms[$real_tax_name][] = $term_list;
+					}
+				}
+			}
+			return $terms;
+		});
+
+		if($is_variation){
+			$variation_attributes_raw_list = wc_get_product_variation_attributes($product->variation_id);
+			foreach ($variation_attributes_raw_list as $attribute_name => $attribute_value){
+				$real_tax_name = str_replace('attribute_','',$attribute_name);
+				$term = get_term_by('slug',$attribute_value,$real_tax_name);
+				$variation_attributes[$real_tax_name] = $term;
+			}
+		}
+
+		if($is_variation){
+			$attributes = isset($variation_attributes) ? $variation_attributes : [];
+			$attributes['_parent'] = $main_attributes;
+		}else{
+			$attributes = $main_attributes;
+		}
+
+		return $attributes;
+	}
+
+	/**
 	 * Alias of get_post_meta() that returns the meta unserialized and cache the results.
 	 *
 	 * @param int|object $post if object id provided, it checks
@@ -82,14 +131,14 @@ class WooCommerce{
 		$post_id = false;
 
 		if(!is_numeric($post)){
-			if($post instanceof \WP_Post){
+			if($post instanceof WBF_Product_Variation || $post instanceof \WC_Product_Variation){
+				$post_id = $post->variation_id;
+			}else{
 				if(isset($post->id)){
 					$post_id = $post->id;
 				}else{
 					$post_id = $post->ID;
 				}
-			}elseif($post instanceof WBF_Product_Variation || $post instanceof \WC_Product_Variation){
-				$post_id = $post->variation_id;
 			}
 		}else{
 			$post_id = $post;
@@ -100,15 +149,33 @@ class WooCommerce{
 		static $cache;
 		if(isset($cache[$post_id])) return $cache[$post_id];
 
-		$metas = array_map(function($value){
-			if(is_array($value) && isset($value[0])){
-				return maybe_unserialize($value[0]);
-			}else{
-				return $value;
-			}
-		},get_post_meta($post_id));
+		/**
+		 * Get and unserialize post meta
+		 *
+		 * @param $post_id
+		 *
+		 * @return array
+		 */
+		$get_metas = function($post_id){
+			$metas = array_map(function($value){
+				if(is_array($value) && isset($value[0])){
+					return maybe_unserialize($value[0]);
+				}else{
+					return $value;
+				}
+			},get_post_meta($post_id));
+			return $metas;
+		};
+
+		$metas = $get_metas($post_id);
 
 		$cache[$post_id] = $metas;
+
+		if($post instanceof \WC_Product_Variation || $post instanceof WBF_Product_Variation){
+			$parent_metas = $get_metas($post->parent->id);
+
+			$cache[$post_id]['__parent'] = $parent_metas;
+		}
 
 		return $metas;
 	}
