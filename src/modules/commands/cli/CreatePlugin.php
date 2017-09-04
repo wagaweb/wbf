22b3565887e564be644a1ce3d529ec856765e5b3
lostcore@gmail.com <123qw45>
2017-09-04 18:30:33 +0200

    skeleton

--- src/modules/commands/cli/CreatePlugin.php
+++ src/modules/commands/cli/CreatePlugin.php
@@ -7,9 +7,17 @@ use WBF\modules\commands\BaseCommand;
 class CreatePlugin extends BaseCommand {
 	public function configure() {
 		$this->set_name('wbf:create-plugin');
+		$this->set_shortdesc("Create e new WBF-compatible plugin.");
+		$this->set_synopsis("
+		* ## OPTIONS'
+		* 
+		* <type>,
+		* : The type of the plugin to create (eg: simple)
+		* ");
 	}
 
-	public function __invoke() {
-		parent::__invoke();
+	public function __invoke($args) {
+		$type = $args[0];
+		\WP_CLI::success($args[0]);
 	}
 }
