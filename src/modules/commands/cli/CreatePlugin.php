7976537ee6e9cd2fcd8d4d22aa40d1d61e13af39
LostCore <lostcore@gmail.com>
2017-09-05 13:01:35 +0200

    Synopsis

--- src/modules/commands/cli/CreatePlugin.php
+++ src/modules/commands/cli/CreatePlugin.php
@@ -8,15 +8,16 @@ class CreatePlugin extends BaseCommand {
 	public function configure() {
 		$this->set_name('wbf:create-plugin');
 		$this->set_shortdesc("Create e new WBF-compatible plugin.");
-		$this->set_synopsis("
-		* ## OPTIONS'
-		* 
-		* <type>,
-		* : The type of the plugin to create (eg: simple)
-		* ");
+		$this->set_synopsis([
+			[
+				'type' => 'positional',
+				'name' => 'type',
+				'description' => 'The plugin type to create'
+			]
+		]);
 	}
 
-	public function __invoke($args) {
+	public function __invoke($args,$assoc_args) {
 		$type = $args[0];
 		\WP_CLI::success($args[0]);
 	}
