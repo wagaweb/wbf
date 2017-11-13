df93dbc1eaddbeaee5174ded5fd07740cbec8926
lostcore@gmail.com <123qw45>
2017-11-13 18:28:25 +0100

    Initial

--- src/modules/plugins_options/bootstrap.php
+++ src/modules/plugins_options/bootstrap.php
@@ -3,5 +3,8 @@
 namespace WBF\modules\plugins_options;
 
 add_action('wbf_admin_submenu', function(){
+	if(has_filter('wbf/modules/plugins_options/tabs')){
+		$tabs = apply_filters('wbf/modules/plugins_options/tabs',[]);
 
+	}
 });
