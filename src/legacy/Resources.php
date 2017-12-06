e87cf9e150a2f7bf703b21b2582b1edd34663618
lostcore@gmail.com <123qw45>
2017-12-06 10:52:57 +0100

    Fixed legacy compatibility

--- src/legacy/Resources.php
+++ src/legacy/Resources.php
@@ -3,8 +3,8 @@
 namespace WBF\legacy;
 
 class Resources{
-	public function get_working_directory(){
-		return WBF()->get_working_directory();
+	public function get_working_directory($base = false){
+		return WBF()->get_working_directory($base);
 	}
 	public function get_working_directory_uri($base = false){
 		return WBF()->get_working_directory_uri($base);
