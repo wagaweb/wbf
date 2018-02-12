ec1a5af8a11b0df66c003e7e14779bdd78ee2c39
LostCore <lostcore@gmail.com>
2018-02-12 13:17:20 +0100

    create_table function

--- src/components/utils/DB.php
+++ src/components/utils/DB.php
@@ -30,4 +30,30 @@ class DB {
 		$cache[$table_name] = false;
 		return false;
 	}
+
+	/**
+	 * @param $table_name
+	 * @param $fields
+	 * @param array $primary_keys
+	 *
+	 * @return bool
+	 */
+	static function create_table($table_name,$fields,$primary_keys = []){
+		global $wpdb;
+		$wpdb->suppress_errors();
+		$charset_collate = $wpdb->get_charset_collate();
+		$sql = 'CREATE TABLE `'.$wpdb->prefix.$table_name.'` (';
+		foreach ($fields as $field_name => $field_args){
+			$sql .= '`'.$field_name.'` '.$field_args.' ,';
+		}
+		if(!empty($primary_keys)){
+			$sql .= 'PRIMARY KEY (`'.implode('`,`',$primary_keys).'`)';
+		}else{
+			$sql = rtrim($sql,',');
+		}
+		$sql .= ') '.$charset_collate.';';
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta($sql);
+		return self::table_exists($table_name);
+	}
 }
