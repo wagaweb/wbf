069ea6477c1729f54f1b01e8d1bce5a8d5506bff
Riccardo D'Angelo <riccardo@waga.it>
2018-08-03 12:44:12 +0200

    small ref

--- src/components/utils/CLI.php
+++ src/components/utils/CLI.php
@@ -13,41 +13,55 @@ class CLI{
 	 * @param string $command
 	 * @param string $executable (default: wp)
 	 * @param string $exec_function (default: exec)
-	 * @param null &$output the variable in which put the command output (for exec and passthru)
-	 * @param null &$return_var the variable in which put the command return status (for exec and passthru)
 	 *
-	 * @return mixed the $exec_function output
+	 * @return array
 	 * @throws \Exception
 	 */
-	public static function exec_wp_cli_command($command,$executable='wp',$exec_function = self::EXEC_FUNCTION_EXEC,&$output = null,&$return_var = null){
+	public static function exec_wp_cli_command($command,$executable='wp',$exec_function = self::EXEC_FUNCTION_EXEC){
 		$cmd = 'cd '.ABSPATH;
 		$cmd .= ' &&';
 		$cmd .= ' '.$executable;
 		$cmd .= ' '.$command;
-		return self::perform_command($cmd,$exec_function,$output,$return_var);
+		return self::perform_command($cmd,$exec_function);
 	}
 
 	/**
-	 * Executes a script or a command within the theme directory.
+	 * Executes a command from the theme directory
 	 *
 	 * @param string $command
-	 * @param string $relative_dir (default empty string)
-	 * @param bool $null_output append ' > /dev/null 2>/dev/null &' to the command.
+	 * @param string $relative_path (additional path to append to the theme directory)
 	 * @param string $exec_function (default: exec)
-	 * @param null &$output the variable in which put the command output (for exec and passthru)
-	 * @param null &$return_var the variable in which put the command return status (for exec and passthru)
+	 * @param bool $no_output whether append ' > /dev/null 2>/dev/null &' to the command or not
 	 *
-	 * @return mixed the $exec_function output
+	 * @return array
 	 * @throws \Exception
 	 */
-	public static function exec_cli_script($command,$relative_dir='',$null_output=false,$exec_function=self::EXEC_FUNCTION_EXEC,&$output= null,&$return_var= null){
-		$cmd = 'cd '.get_stylesheet_directory().$relative_dir;
+	public static function exec_cli_command_from_theme_directory($command,$relative_path='',$exec_function=self::EXEC_FUNCTION_EXEC,$no_output=false){
+		return self::exec_cli_command($command,get_stylesheet_directory().$relative_path,$exec_function,$no_output);
+	}
+
+	/**
+	 * Executes a command from the specified $wd
+	 *
+	 * @param string $command
+	 * @param string $wd the working directory (default to ABSPATH)
+	 * @param string $exec_function (default: exec)
+	 * @param bool $no_output whether append ' > /dev/null 2>/dev/null &' to the command or not
+	 *
+	 * @return array
+	 * @throws \Exception
+	 */
+	public static function exec_cli_command($command,$wd=null,$exec_function=self::EXEC_FUNCTION_EXEC,$no_output=false){
+		if(!$wd){
+			$wd = ABSPATH;
+		}
+		$cmd = 'cd '.$wd;
 		$cmd .= ' &&';
 		$cmd .= ' '.$command;
-		if($null_output){
+		if($no_output){
 			$cmd .= ' > /dev/null 2>/dev/null &';
 		}
-		return self::perform_command($cmd,$exec_function,$output,$return_var);
+		return self::perform_command($cmd,$exec_function);
 	}
 
 	/**
@@ -55,30 +69,38 @@ class CLI{
 	 *
 	 * @param string $cmd
 	 * @param string $exec_function
-	 * @param null $output
-	 * @param null $return_var
 	 *
-	 * @return bool|string
+	 * @return array
 	 * @throws \Exception
 	 */
-	private static function perform_command($cmd,$exec_function,&$output=null,&$return_var=null){
-		if(\function_exists($exec_function)){
-			switch($exec_function){
-				case self::EXEC_FUNCTION_SYSTEM:
-					return system($cmd,$exec_arg1);
-					break;
-				case self::EXEC_FUNCTION_EXEC:
-					return exec($cmd,$output,$return_var);
-					break;
-				case self::EXEC_FUNCTION_PASSTHRU:
-					passthru($cmd,$return_var);
-					return true;
-					break;
-				default:
-					return $exec_function($cmd);
-			}
-		}else{
-			throw new \Exception('Invalid $exec_function provided');
+	private static function perform_command($cmd,$exec_function){
+		switch($exec_function){
+			case self::EXEC_FUNCTION_SYSTEM:
+				$status = '';
+				$r = system($cmd,$status);
+				return [
+					'output' => $r,
+					'status' => $status
+				];
+				break;
+			case self::EXEC_FUNCTION_EXEC:
+				$output = '';
+				$status = '';
+				$r = exec($cmd,$output,$return_var);
+				return [
+					'output' => $output,
+					'status' => $status
+				];
+				break;
+			case self::EXEC_FUNCTION_PASSTHRU:
+				$status = '';
+				passthru($cmd,$status);
+				return [
+					'status' => $status
+				];
+				break;
+			default:
+				throw new \Exception('Invalid function provided');
 		}
 	}
 }
