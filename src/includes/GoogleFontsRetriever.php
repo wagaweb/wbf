b8d153aed08a00a01cc9e0743f9a90699eb14b5b
Riccardo D'Angelo <lostcore@gmail.com>
2018-12-14 14:43:25 +0100

    some minor refinements

--- src/includes/GoogleFontsRetriever.php
+++ src/includes/GoogleFontsRetriever.php
@@ -3,23 +3,23 @@
 namespace WBF\includes;
 
 class GoogleFontsRetriever{
-    const api_url = "https://www.googleapis.com/webfonts/v1/webfonts";
+    const api_url = 'https://www.googleapis.com/webfonts/v1/webfonts';
 	/**
 	 * @var string|null
 	 */
-    private $api_key = null;
+    private $api_key;
 	/**
 	 * @var string|null
 	 */
-    var $last_error = null;
+	public $last_error ;
 	/**
 	 * @var string
 	 */
-    var $cache_file_name = "wbf_font_cache.php";
+    public $cache_file_name = 'wbf_font_cache.php';
 	/**
 	 * @var \stdClass|null
 	 */
-	var $cached_fonts = null;
+	public $cached_fonts;
 
 	/**
 	 * GoogleFontsRetriever constructor.
@@ -27,6 +27,7 @@ class GoogleFontsRetriever{
 	 * @param string|null $api_key
 	 * @param string|null $cache_file_name
 	 *
+	 * @throws GoogleFontsRetrieverException
 	 * @throws \Exception
 	 */
 	public function __construct($api_key = null,$cache_file_name = null){
@@ -53,7 +54,7 @@ class GoogleFontsRetriever{
 	 * @return bool
 	 */
     public function can_download_webfonts(){
-		return isset($_GET['wbf_update_font_cache']) && isset($this->api_key);
+		return isset( $_GET['wbf_update_font_cache'], $this->api_key );
     }
 
 	/**
@@ -69,14 +70,16 @@ class GoogleFontsRetriever{
     }
 
 	/**
-	 * @param $familyname
+	 * @param string $familyName
 	 *
 	 * @return bool
 	 */
-	function get_properties_of($familyname){
-		if(!isset($this->cached_fonts)) return false;
+	public function get_properties_of($familyName){
+		if(!isset($this->cached_fonts)){
+			return false;
+		}
 		foreach($this->cached_fonts->items as $font){
-			if($font->family == $familyname){
+			if( $font->family === $familyName){
 				return $font;
 			}
 		}
@@ -85,22 +88,26 @@ class GoogleFontsRetriever{
 
 	/**
 	 * @return \stdClass|bool
-	 * @throws \Exception
 	 */
-	function read_font_cache_file(){
-		$cache_file = WBF()->get_working_directory()."/gfont_font_cache/".$this->cache_file_name;
-		if(!\is_file($cache_file) || !\is_readable($cache_file)){
-			$cache_file = WBF()->get_path()."cache/".$this->cache_file_name;
-		}
-		if(\is_file($cache_file) && \is_readable($cache_file)){
-			require_once $cache_file;
-			if(isset($fonts)){
-				$fonts_json = $fonts;
-				$fonts_parsed = json_decode($fonts_json);
-				if($fonts_parsed instanceof \stdClass){
-					return $fonts_parsed;
+	public function read_font_cache_file(){
+		try{
+			$cache_file = WBF()->get_working_directory() . '/gfont_font_cache/' . $this->cache_file_name;
+			if(!\is_file($cache_file) || !\is_readable($cache_file)){
+				$cache_file = WBF()->get_path() . 'cache/' . $this->cache_file_name;
+			}
+			if(\is_file($cache_file) && \is_readable($cache_file)){
+				require_once $cache_file;
+				global $fonts;
+				if(isset($fonts)){
+					$fonts_json = $fonts;
+					$fonts_parsed = json_decode($fonts_json);
+					if($fonts_parsed instanceof \stdClass){
+						return $fonts_parsed;
+					}
 				}
 			}
+		}catch (\Exception $e){
+			return false;
 		}
 		return false;
 	}
@@ -110,7 +117,7 @@ class GoogleFontsRetriever{
 	 *
 	 * @throws \Exception
 	 */
-    function write_font_cache_file($fonts_json = '{}'){
+    public function write_font_cache_file($fonts_json = '{}'){
 		if(!\is_string($fonts_json)){
 			$fonts_json = '{}';
 		}
@@ -124,12 +131,12 @@ class GoogleFontsRetriever{
 		//File initialization
         $cache_file_content = '<?php $fonts = \''.$fonts_json.'\'; ?>';
         wp_mkdir_p(WBF()->get_working_directory().'/gfont_font_cache');
-	    $cache_file = WBF()->get_working_directory()."/gfont_font_cache/".$this->cache_file_name;
+	    $cache_file = WBF()->get_working_directory().'/gfont_font_cache/'.$this->cache_file_name;
 
 	    //File writing
 	    $fhandle = fopen($cache_file,'w');
         if(fwrite($fhandle, $cache_file_content) === FALSE) {
-            $this->last_error = new GoogleFontsRetrieverException("Unable to write the font cache file, located at: $cache_file","file_write_failed");
+            $this->last_error = new GoogleFontsRetrieverException("Unable to write the font cache file, located at: $cache_file",'file_write_failed');
         }
         fclose($fhandle);
     }
@@ -138,28 +145,37 @@ class GoogleFontsRetriever{
 	 * Download webfonts json from google
 	 *
 	 * @return bool|string
+	 * @throws GoogleFontsRetrieverException
 	 */
-    function download_webfonts(){
+    public function download_webfonts(){
 	    $fonts_json = false;
 	    if(!\function_exists('\wp_remote_get')){
 		    return $fonts_json;
 	    }
+
 	    $url = \add_query_arg([
-		    "key" => $this->api_key
+		    'key' => $this->api_key
 	    ],self::api_url);
+
 	    $response = \wp_remote_get($url, array('sslverify' => false));
+
 	    if(\is_wp_error($response)){
-		    $this->last_error = new GoogleFontsRetrieverException(__("Unable to connect to Google API"), "connection_failed");
-	    }else{
-		    if(isset($response['body']) && $response['body']){
-			    if(\strpos($response['body'], 'error') === false){
-				    $fonts_json = $response['body'];
-			    }else{
-				    $error = \json_decode($response['body']);
-				    $this->last_error = new GoogleFontsRetrieverException(\sprintf(__('Google API Notice: %s. %s', "wbf"), $error->error->code, $error->error->message), "limit_reached");
-			    }
+	    	$e = new GoogleFontsRetrieverException(__( 'Unable to connect to Google API' ), 'connection_failed' );
+		    $this->last_error = $e;
+		    throw $e;
+	    }
+
+	    if(isset($response['body']) && $response['body']){
+		    if(\strpos($response['body'], 'error') === false){
+			    $fonts_json = $response['body'];
+		    }else{
+			    $error = \json_decode($response['body']);
+			    $e = new GoogleFontsRetrieverException(\sprintf(__('Google API Notice: %s. %s', 'wbf'), $error->error->code, $error->error->message), 'limit_reached' );
+			    $this->last_error = $e;
+			    throw $e;
 		    }
 	    }
+
 	    return $fonts_json;
     }
 }
